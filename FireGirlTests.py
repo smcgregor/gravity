32f9c4e844e7baa354e5cd55e7ac3e137f824066
hailey <hailey.buckingham@oregonstate.edu>
2015-04-23 20:47:30 -0700

    Added Stats Test

--- FireGirlTests.py
+++ FireGirlTests.py
@@ -1,4 +1,5 @@
 from FireGirlOptimizer import *
+from FireGirlStats import *
 
 class FireGirlTests:
     #This class is designed to house functions useful in testing and analyzing the behavior
@@ -12,6 +13,78 @@ class FireGirlTests:
         self.PRINT_DETAILS = True
         #FLAG: REGARDLESS OF OTHER FLAGS, print no ouptuts
         self.SILENT = False
+
+    def stats_test(self, pathway_count = 20, years=100, start_ID=0):
+        """Short test of the FireGirlStats functions
+
+        Note: At the moment, a coin-toss policy is being used...
+        """
+
+        if not self.SILENT:
+            if (self.PRINT_DETAILS or self.PRINT_SUMMARIES):
+                print(" ")
+                print("STATS TEST:")
+
+        opt = FireGirlPolicyOptimizer()
+        opt.SILENT = True
+        opt.createFireGirlPathways(pathway_count, years, start_ID)
+
+        #running stat functions
+        sup_costs = suppression_cost_stats_by_year(opt.pathway_set)
+        harvests = timber_harvest_stats_by_year(opt.pathway_set)
+        growth = growth_stats_by_year(opt.pathway_set)
+        fires = fire_stats_by_year(opt.pathway_set)
+
+
+        if not self.SILENT:
+            if self.PRINT_DETAILS:
+                print("Suppression Cost Stats")
+                print("ave, max, min, std, 95%high, 95%low")
+                for i in range(len(sup_costs[0])):
+                    #suppresion_cost_stats_by_year returns 6 lists
+                    for j in range(6):
+                        print(str(sup_costs[j][i]) + ","), #comma tells python not to end the line
+                    #now end the line
+                    print(" ")
+
+                print(" ")
+                print("Harvest Value Stats")
+                print("ave, max, min, std, 95%high, 95%low")
+                for i in range(len(harvests[0])):
+                    #timber_harvest_stats_by_year returns 6 lists
+                    for j in range(6):
+                        print(str(harvests[j][i]) + ","), #comma tells python not to end the line
+                    #now end the line
+                    print(" ")
+
+                print(" ")
+                print("Timber Growth Stats")
+                print("ave, max, min, std, 95%high, 95%low")
+                for i in range(len(growth[0])):
+                    #growth_stats_by_year returns 6 lists
+                    for j in range(6):
+                        print(str(growth[j][i]) + ","), #comma tells python not to end the line
+                    #now end the line
+                    print(" ")
+
+                print(" ")
+                print("Fire Stats:")
+                print("Cells Burned, , , , , , Timber Lost, , , , , , Suppress Decisions")
+                print("ave, max, min, std, 95%high, 95%low, ave, max, min, std, 95%high, 95%low")
+                for i in range(len(fires[0][0])):
+                    #fire_stats_by_year returns 3 lists
+                    # the first is a sub-list of 6 stats for CELLS BURNED, as the features above
+
+                    #printing Cells burned stats
+                    for j in range(6):
+                        print(str(fires[0][j][i]) + ","), #comma tells python not to end the line
+                    #printing timber lost stats
+                    for j in range(6):
+                        print(str(fires[1][j][i]) + ","), #comma tells python not to end the line
+                    #printing suppress decisions stat and ending the line
+                    print(str(fires[2][i]))
+
+
 
 
     def monte_carlo_baselines(self, pathway_count=5, years=100, start_ID=2000):
@@ -108,6 +181,23 @@ class FireGirlTests:
 
 
         return [ave_let_burn, ave_suppress_all, ave_coin_toss]
+
+class FireGirlTrials:
+    """This class contains member functions that produce test data as outputs, replacing test scripts
+
+    """
+
+    def __init__():
+        """Initialization function: Instantiates a FireGirlPolicyOptimizer and FireGirlPolicy for trials to use.
+
+        """
+        self.Opt = FireGirlPolicyOptimizer()
+        self.Policy = FireGirlPolicy()
+
+
+
+
+
 
 
 #setting up service-style tests. This will activate if you issue: "python FireGirlTests.py" at a command line, but
